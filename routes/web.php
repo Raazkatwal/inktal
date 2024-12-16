77a662e869ca281ba47e9a51beb919b53a8eb268
Raaz <raazkatwal5@gmail.com>
2024-12-16 16:32:56 +0545

    testing git

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 include base_path('routes/admin.php');
-include base_path('routes/website.php');
-include base_path('routes/payment.php');
+// include base_path('routes/website.php');
+// include base_path('routes/payment.php');
 
 Route::fallback(function () {
     if (request()->is('419')) {
